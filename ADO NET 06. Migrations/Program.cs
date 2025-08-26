288d8921f12429bc578b02e98866ce7315e90121
Nadir Zamanov <Zamanov@itstep.edu.az>
2025-08-26 15:47:37 +0400

    Migrations

---
+++ ADO NET 06. Migrations/Program.cs
@@ -0,0 +1,22 @@
+﻿// Migrations
+using StudentContext db = new();
+
+List<Student> students = new()
+{
+    new Student()
+    {
+        FirstName = "Co",
+        LastName = "Bayden",
+        Age = 92
+    },
+    new Student()
+    {
+        FirstName = "Donald",
+        LastName = "Tramp",
+        Age = 22
+    }
+};
+
+db.Students.AddRange(students);
+
+db.SaveChanges();
---
+++ ADO NET 06. Migrations/Student.cs
@@ -0,0 +1,9 @@
+﻿// 
+class Student
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public int Age { get; set; }
+    //public bool Gender { get; set; }
+}
