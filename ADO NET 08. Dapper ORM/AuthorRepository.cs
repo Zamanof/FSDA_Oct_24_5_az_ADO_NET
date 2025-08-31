f10cb43a9bcd7fdb3cdac6b372774d8ed7c8593f
Nadir Zamanov <Zamanov@itstep.edu.az>
2025-08-31 17:07:08 +0400

    Dapper Intro

---
+++ ADO NET 08. Dapper ORM/AuthorRepository.cs
@@ -0,0 +1,65 @@
+﻿
+using Dapper;
+using System.Data;
+
+namespace ADO_NET_08._Dapper_ORM;
+
+class AuthorRepository : IAuthorRepository
+{
+    private IDbConnection _db;
+
+    public AuthorRepository(IDbConnection db, string connectionString)
+    {
+        _db = db;
+        _db.ConnectionString = connectionString;
+    }
+
+    public Author AddAuthor(Author author)
+    {
+        var sqlQuery = 
+            @"INSERT INTO Author(FirstName, LastName)
+              VALUES (@FirstName, @LastName)
+              SELECT CAST(SCOPE_IDENTITY() AS int)";
+
+        var id = _db.Query<int>(sqlQuery, 
+            new {
+                @FirstName = author.FirstName,
+                @LastName = author.LastName
+            }).FirstOrDefault();
+        
+        author.Id = id;
+        
+        return author;
+    }
+
+    public void AddAuthors(IEnumerable<Author> authors)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Author GetAuthorById(int id)
+    {
+        var sqlQuery = "SELECT * FROM Author WHERE Id=@Id";
+        return _db.QueryFirstOrDefault<Author>(sqlQuery, new
+        {
+            @Id=id
+        })!;
+    }
+
+    public IEnumerable<Author> GetAuthors()
+    {
+        var sqlQuery = @"SELECT * FROM Author";
+        return _db.Query<Author>(sqlQuery);
+    }
+
+    public void RemoveAuthorById(int id)
+    {
+        var sqlQuery = "DELETE Author WHERE Id=@id";
+        _db.Execute(sqlQuery, new { @id = id });
+    }
+
+    public void RemoveAuthorsById(int[] ids)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ ADO NET 08. Dapper ORM/Program.cs
@@ -0,0 +1,28 @@
+﻿// Dapper ORM
+using ADO_NET_08._Dapper_ORM;
+using Microsoft.Data.SqlClient;
+
+
+Console.WriteLine();
+var connectionString = @"Server=(localdb)\MSSQLLocalDB;Database=Authors;Integrated Security=True;Trust Server Certificate=True;";
+
+IAuthorRepository repository = new AuthorRepository(new SqlConnection(), connectionString);
+
+#region Add Data
+//Author author = new() { FirstName = "Dan", LastName = "Brown" };
+//repository.AddAuthor(author);
+#endregion
+
+#region Read Datas
+//var authors = repository.GetAuthors().ToList();
+//authors.ForEach(Console.WriteLine);
+#endregion
+
+#region Get Author By Id
+//var author = repository.GetAuthorById(2);
+//Console.WriteLine(author);
+#endregion
+
+#region Remove Data by Id
+repository.RemoveAuthorById(3);
+#endregion
